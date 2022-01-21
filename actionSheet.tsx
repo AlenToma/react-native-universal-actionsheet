215fc75b9cf1f35e35c5ab7739d9d44d317cdae1
Alen Toma <alen.toma@gmail.com>
2022-01-21 13:23:22 +0100

    Add package

--- actionSheet.tsx
+++ actionSheet.tsx
@@ -13,7 +13,7 @@ import {
   PanResponder,
   PanResponderInstance,
 } from 'react-native';
-
+import 'react-native-get-random-values';
 import { v4 as uuidv4 } from 'uuid';
 
 const ActionSheetProviderContext = React.createContext(
